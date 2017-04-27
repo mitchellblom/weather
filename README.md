e12010837dd836b41de58ba423b04d574191da43
Mitchell Blom <mitchellblom@gmail.com>
2017-04-27 12:57:40 -0500

    setting up for api call

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # NSS Weather API Exercise
+
+![Splashpage]()
+
+<hr>
+
+## Summary
+Connecting to an online **API** using an access key. Calling the data via **AJAX** and presenting it in a user-friendly format styled with basic **SASS CSS**. Continued use of **ES6** syntax.
+
+<hr>
+
+## To Access Necessary Libaries:
+ - Pull down project
+ - cd into lib
+ - Run command "bower install"
+ - Run command "npm install"
+
+
+<hr>
+
+## Requirements
+1. 
+
+Your code should:
+
+ - 
+
+<hr>
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,34 @@
+$(document).ready(function(){
+
 console.log("main js working");
 
-$("#something").hide()
+const apiKey = '';								// key goes here
+
+	$('body').on('click', 'li', (e) => {
+		console.log(e.target.innerHTML);
+		loadCity(e.target.innerHTML).then((data) => {			// upon change, adjust origin line for reference
+			console.log(data);
+		});
+		// loadPlaces(e.target.innerHTML).then((data) => {
+		// 	results = data.results;
+		// 		writePlaceToDom(results);
+		// 	})
+		// .catch((error) => {
+		// 	console.log(error);
+		// });
+	});
+
+	const loadCity = (city) => {
+		return new Promise ((resolve, reject) => {
+			$.ajax(`api.openweathermap.org/data/2.5/forecast?id=524901&APPID=1111111111 
+			`)
+			.done((data) => {
+				resolve(data.result);		// this result is getting back to line 8 for the then()
+			})
+			.fail((error) => {
+				reject(error);
+			});
+		});
+	};
+
+});
