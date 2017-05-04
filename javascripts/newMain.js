6b27def30cb5649fa72ceadbb8daeb9c0d152d17
Mitchell Blom <mitchellblom@gmail.com>
2017-05-03 22:37:26 -0500

    api keys hooked up

--- javascripts/newMain.js
+++ javascripts/newMain.js
@@ -18,10 +18,49 @@ $(function() {
 
 	FbApi.firebaseCredentials().then((keys) => {
         apiKeys = keys;
+        console.log("api keys: ", apiKeys);
         firebase.initializeApp(apiKeys);
-        FbApi.writeDom(apiKeys);
+        // FbApi.writeDom(apiKeys);
     }).catch((error) => {
         console.log("key errors", error);
     });
+
+// register new user
+
+    $("#registerButton").click(() => {
+        let email = $("#inputEmail").val();
+        let password = $("#inputPassword").val();
+        let username = $("#inputUsername").val();
+        let user = {
+            email,
+            password
+        };
+        FbApi.registerUser(user).then((response) => {
+            let newUser = {
+                uid: response.uid,
+                username: username
+            };
+            FbApi.addUser(apiKeys, newUser).then((response) => {
+                FbApi.loginUser(user).then((response) => {
+                    clearLogin();
+                    $('#login-container').addClass('hide');
+                    $('.main-container').removeClass('hide');
+                    // FbApi.writeDom(apiKeys);
+                }).catch((error) => {
+                    console.log("error in loginUser: ", error);
+                });
+            }).catch((error) => {
+                console.log("error in addUser", response);
+            });
+        }).catch((error) => {
+            console.log("error in registerUser", error);
+        });
+    });
+
+    let clearLogin = () => {
+        let email = $("").val();
+        let password = $("").val();
+        let username = $("").val();
+    };
 
 });
