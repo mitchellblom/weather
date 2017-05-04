15531cd45f6706c5583f253bd697b9177204949a
Mitchell Blom <mitchellblom@gmail.com>
2017-05-03 20:55:52 -0500

    style fix and added newMain

---
+++ javascripts/newMain.js
@@ -0,0 +1,13 @@
+$(function() {
+
+	let apiKeys = {};
+
+	FbApi.firebaseCredentials().then((keys) => {
+        apiKeys = keys;
+        firebase.initializeApp(apiKeys);
+        FbApi.writeDom(apiKeys);
+    }).catch((error) => {
+        console.log("key errors", error);
+    });
+
+});
